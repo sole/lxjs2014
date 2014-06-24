120c80097167f247c574dba80f6520cf1bd8c983
sole <sole@mozilla.com>
2014-06-24 11:42:07 +0200

    creating & inserting code button

--- src/examples/js/x-livecode/x-livecode.js
+++ src/examples/js/x-livecode/x-livecode.js
@@ -11,7 +11,7 @@
 		var src = this.hasAttribute('src') ? attr.src.value : null;
 		var showcode = this.hasAttribute('showcode');
 		var autoxec = this.hasAttribute('autoexec');
-		var appendButtonSelector = this.hasAttribute('appendbutton') ? attr.appendbutton : null;
+		var appendButtonSelector = this.hasAttribute('appendbutton') ? attr.appendbutton.value : null;
 
 		// layout
 		var divEditor = document.createElement('div');
@@ -21,18 +21,22 @@
 		editor.setAttribute('src', src);
 		divEditor.appendChild(editor);
 
+		if(appendButtonSelector !== null) {
+			var button = document.createElement('button');
+			var insertionPoint = document.querySelector(appendButtonSelector);
+			button.innerHTML = 'code';
+			insertionPoint.appendChild(button);
+		}
+
 		// If we have children bring them to the new divLive layer
 		while(this.childElementCount) {
 			var child = this.childNodes[0];
-			console.log('relocating child', child);
 			divLive.appendChild(child);
 		}
 
 		// Now append the new divs here
 		this.appendChild(divEditor);
 		this.appendChild(divLive);
-
-
 
 	};
 
